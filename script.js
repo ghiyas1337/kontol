edec3953d95e0b478e5cddc73cd43c4b6c427b7e
ghiyas1337 <ghiyasalok@gmail.com>
2024-12-15 11:18:29 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('showFaceButton').addEventListener('click', function() {
+    const faceContainer = document.getElementById('faceContainer');
+    faceContainer.classList.toggle('hidden'); // Menampilkan atau menyembunyikan gambar
+});
