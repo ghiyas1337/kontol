c9b70e3ad7ccbd5c7651d3d047c9fd39ce33032f
ghiyas1337 <ghiyasalok@gmail.com>
2024-12-16 09:32:52 +0700

    Update fireworks.js

--- fireworks.js
+++ fireworks.js
@@ -58,8 +58,8 @@ function createParticle(x, y, isSparkle) {
 	if (!isSparkle) {
 			const img = document.createElement("img");
 			img.src = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-			img.style.width = "30px";
-			img.style.height = "30px";
+			img.style.width = "70px";
+			img.style.height = "70px";
 			el.appendChild(img);
 	} else {
 			el.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
